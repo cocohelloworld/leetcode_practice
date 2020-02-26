d911631718b02cc431990ea9d3ec3d677febaa8d
wrld <974186763@qq.com>
2020-02-26 10:18:51 +0800

    Add practice_melon/question_8.cpp

--- practice_melon/question_7.cpp
+++ practice_melon/question_7.cpp
@@ -1,15 +1,17 @@
 class Solution {
-public:
-    int fib(int n) {
-        long long f[101];
-        f[0]=0;
-        f[1]=1;
-        if(n==0)return 0;
-        if(n==1)return 1;
-        else
-        for(int i =2;i<=n;i++){
-            f[i]=(f[i-1]+f[i-2])%1000000007;//中间计算斐波那契数列取余数和计算完再取余数结果相同
-        }
-        return f[n];
-    }
+ public:
+  int fib(int n) {
+    long long f[101];
+    f[0] = 0;
+    f[1] = 1;
+    if (n == 0) return 0;
+    if (n == 1)
+      return 1;
+    else
+      for (int i = 2; i <= n; i++) {
+        f[i] = (f[i - 1] + f[i - 2]) %
+               1000000007;  //中间计算斐波那契数列取余数和计算完再取余数结果相同
+      }
+    return f[n];
+  }
 };
